2168525c2eb8676ab80bdd08b9e87292cc26407c
crivcigu <cristhian.po45@gmail.com>
2025-02-25 16:46:39 -0500

    Primer punto hecho

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+function asignarTextoElemento (elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function agregarAmigo (){
+    let inputAmigo = document.getElementById('amigo').value;
+    if (inputAmigo == ''){
+        alert('Debes ingresar un nombre');
+    }else{
+        amigos.push(inputAmigo);
+        let listaAmigos = document.getElementById('listaAmigos');
+        let nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = inputAmigo;
+        listaAmigos.appendChild(nuevoElemento);
+    }
+    limpiar();
+    console.log(amigos);
+    asignarTextoElemento('p', inputAmigo);
+    return;
+
+}
+
+function limpiar(){
+    document.querySelector('#amigo').value = '';
+
+}
+
+agregarAmigo();
